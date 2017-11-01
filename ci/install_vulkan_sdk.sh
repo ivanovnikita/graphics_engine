59cf743ae023f81c5c6ef130ffdef2879c8814b0
Nikita Ivanov <it.naivanov@gmail.com>
2017-11-01 15:11:35 +0300

    install vulkan sdk

---
+++ ci/install_vulkan_sdk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+VULKAN_VERSION="1.0.61.1"
+mkdir vulkan
+cd vulkan
+wget -nv "https://vulkan.lunarg.com/sdk/download/$VULKAN_VERSION/linux/vulkansdk-linux-x86_64-$VULKAN_VERSION.run"
+chmod ugo+x vulkansdk-linux-x86_64-$VULKAN_VERSION.run
+./vulkansdk-linux-x86_64-$VULKAN_VERSION.run
+cd "VulkanSDK/$VULKAN_VERSION"
+export VULKAN_SDK=$PWD/x86_64
+export PATH=$VULKAN_SDK/bin:$PATH
+export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH
+export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
