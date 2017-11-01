0d5ecb0c58f2ce5d79dfc0ff63dc1bb2402054f4
Nikita Ivanov <it.naivanov@gmail.com>
2017-11-01 15:29:29 +0300

    move vulkan sdk installation to another sh script

--- ci/travis_build.sh
+++ ci/travis_build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+set -e
+
+VULKAN_VERSION="1.0.61.1"
+mkdir vulkan
+cd vulkan
+wget -nv "https://vulkan.lunarg.com/sdk/download/$VULKAN_VERSION/linux/vulkansdk-linux-x86_64-$VULKAN_VERSION.run"
+chmod ugo+x vulkansdk-linux-x86_64-$VULKAN_VERSION.run
+./vulkansdk-linux-x86_64-$VULKAN_VERSION.run
+cd "VulkanSDK/$VULKAN_VERSION"
+source setup-env.sh
+
+cd ../../..
 mkdir build && cd build
 cmake ..
 make
