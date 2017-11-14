d252a5c95226006a71d875839f8f008b13415c27
Nikita Ivanov <it.naivanov@gmail.com>
2017-11-14 10:55:01 +0300

    write exception message

--- test/graphics_engine.cpp
+++ test/graphics_engine.cpp
@@ -2,7 +2,19 @@
 
 #include "ge/graphics_engine.h"
 
+#include <iostream>
+
 TEST(GraphicsEngine, create)
 {
-    ASSERT_NO_THROW({ge::GraphicsEngine ge;});
+    ASSERT_NO_THROW
+    ({
+        try
+        {
+            ge::GraphicsEngine ge;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    });
 }
