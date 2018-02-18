fafaa530205087d5dfb84494ac081ee20d8491a9
Nikita Ivanov <it.naivanov@gmail.com>
2018-02-18 20:05:30 +0300

    fix build script

--- ci/travis_build.sh
+++ ci/travis_build.sh
@@ -5,7 +5,6 @@ set -e
 conan remote add common https://api.bintray.com/conan/ivanovnikita/common --insert
 conan remote add graphics https://api.bintray.com/conan/ivanovnikita/graphics_engine --insert
 
-cd ../../..
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
