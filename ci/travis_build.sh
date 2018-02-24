13f126aa6da495620f4f60a8e16984ff927d505b
Nikita Ivanov <it.naivanov@gmail.com>
2018-02-24 21:16:49 +0500

    show loader debug log

--- ci/travis_build.sh
+++ ci/travis_build.sh
@@ -5,6 +5,7 @@ set -e
 conan remote add common https://api.bintray.com/conan/ivanovnikita/common --insert
 conan remote add graphics https://api.bintray.com/conan/ivanovnikita/graphics_engine --insert
 
+export VK_LOADER_DEBUG=all
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
