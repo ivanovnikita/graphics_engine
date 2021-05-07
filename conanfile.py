fa35e2569dd88a17df460b72811cb02881cc42bf
Nikita Ivanov <it.naivanov@gmail.com>
2021-05-07 17:53:14 +0300

    write build method in conan receipt

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,16 @@
-from conans import ConanFile
+from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
 
 class GraphicsEngineConan(ConanFile):
+    name = "GraphicsEngine"
+    version = "0.1.0"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "enable_lto": [True, False],
         "enable_debug_layers": [True, False],
+        "build_examples": [True, False],
         "build_tests": [True, False],
         "build_graphics_tests": [True, False],
     }
@@ -15,21 +18,25 @@ class GraphicsEngineConan(ConanFile):
         "shared": False,
         "enable_lto": False,
         "enable_debug_layers": False,
+        "build_examples": False,
         "build_tests": False,
         "build_graphics_tests": False
     }
     generators = "cmake"
 
+    _cmake = None
+
     def configure(self):
         if self.settings.os != "Linux":
             raise ConanInvalidConfiguration("GraphicsEngine is only supported for Linux")
+
+    def build_requirements(self):
+        self.build_requires("shaderc/2019.0")
 
     def requirements(self):
         self.requires.add("vulkan-headers/1.2.154.0 ", private=False)
         self.requires.add("vulkan-loader/1.2.154.0", private=False)
         self.requires.add("glm/0.9.9.7", private=False)
-
-        self.requires.add("shaderc/2019.0", private=True)
 
         if self.settings.os == "Linux":
             self.requires.add("xorg/system", private=False)
@@ -39,3 +46,32 @@ class GraphicsEngineConan(ConanFile):
 
         if self.options.build_tests or self.options.build_graphics_tests:
             self.requires.add("gtest/1.10.0", private=True)
+
+    def source(self):
+        self.run("git clone git@github.com:ivanovnikita/graphics_engine.git")
+        self.run("cd graphics_engine && git checkout %s" % self.version)
+        self.run("cp -r graphics_engine/* . && rm -rf graphics_engine")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+
+        self._cmake.definitions["GE_ENABLE_LTO"] = self.options.enable_lto
+        self._cmake.definitions["GE_ENABLE_DEBUG_LAYERS"] = self.options.enable_debug_layers
+        self._cmake.definitions["GE_BUILD_EXAMPLES"] = self.options.build_examples
+        self._cmake.definitions["GE_BUILD_TESTS"] = self.options.build_tests
+        self._cmake.definitions["GE_BUILD_GRAPHICS_TESTS"] = self.options.build_graphics_tests
+        self._cmake.definitions["GE_USE_LIBC++"] = self.settings.compiler == "clang" and self.settings.compiler.libcxx == "libc++"
+
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
